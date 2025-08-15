1a1c895b75fd9dc330906a4899d77418a1d0813f
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-08-15 09:19:26 +0000

    re-formulated the comment after discussion

--- src/s-core-devcontainer/.devcontainer/s-core-local/on_create_command.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/on_create_command.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# If /var/cache/bazel exists, it was mounted from the host (see devcontainer-feature.json).
-# Hence, we configure Bazel to use this cache directory instead of the default cache (${HOME}/.cache/bazel).
-# This way, Bazel can re-use an existing cache on the host machine.
-# Note that in some scenarios (like codespaces), this is not the case and hence the cache stays in the container.
+# In some scenarios (like CodeSpaces), apparently not all (or even any?)
+# bind mounts to the host are done and /var/cache/bazel does not exist.
+# See devcontainer-feature.json, where this mount is defined.
+# Hence, if /var/cache/bazel exists, it was mounted from the host,
+# and we configure Bazel to use /var/cache/bazel as cache.
+# This way, Bazel re-uses a bind-mounted cache from the host machine, while
+# still using the default cache (${HOME}/.cache/bazel) if no such cache exists.
 if [ -d /var/cache/bazel ]; then
   echo "Configuring Bazel to use /var/cache/bazel as cache..."
   echo "startup --output_user_root=/var/cache/bazel" >> ~/.bazelrc
