372f2c6d0988409bd1a7f0f30decfed146d74525
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-10-06 07:41:10 +0000

    split in two calls, one per platform

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 
 TAG="${1:-latest}"
 
-devcontainer build --push --platform linux/aarch64,linux/amd64 --workspace-folder src/s-core-devcontainer --image-name "ghcr.io/eclipse-score/devcontainer:${TAG}" --cache-from ghcr.io/eclipse-score/devcontainer
+devcontainer build --push --platform linux/aarch64 --workspace-folder src/s-core-devcontainer --image-name "ghcr.io/eclipse-score/devcontainer:${TAG}" --cache-from ghcr.io/eclipse-score/devcontainer
+devcontainer build --push --platform linux/amd64 --workspace-folder src/s-core-devcontainer --image-name "ghcr.io/eclipse-score/devcontainer:${TAG}" --cache-from ghcr.io/eclipse-score/devcontainer
