bb50ddce0569097cc003f502c3f29e13ad8b2c2a
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2025-11-07 12:11:34 +0000

    add bash argument if minimum bazel version is met

--- src/s-core-devcontainer/.devcontainer/s-core-local/install_matching_bazel_version.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/install_matching_bazel_version.sh
@@ -6,7 +6,14 @@ set -eo pipefail
 if [ -f .bazelversion ] && [ "$(cat .bazelversion)" != "$INSTALLED_BAZEL_VERSION" ]; then
     # Pre-install the matching Bazel version, setup the bash command completion
     USE_BAZEL_VERSION=$(cat .bazelversion)
-    bazel help completion > /tmp/bazel-complete.bash
+
+    min_bazel_version_for_bash_option="8.4.0"
+    bash=""
+    if [ "$(printf '%s\n' "$min_bazel_version_for_bash_option" "$USE_BAZEL_VERSION" | sort -V | head -n1)" = "$min_bazel_version_for_bash_option" ]; then
+        bash="bash"
+    fi
+
+    bazel help completion ${bash} > /tmp/bazel-complete.bash
     sudo mv /tmp/bazel-complete.bash /etc/bash_completion.d/bazel-complete.bash
-    echo "export INSTALLED_BAZEL_VERSION=$USE_BAZEL_VERSION" | sudo tee /devcontainer/features/s-core-local/bazel_setup.sh
+    echo "INSTALLED_BAZEL_VERSION=$USE_BAZEL_VERSION" | sudo tee /devcontainer/features/s-core-local/bazel_setup.sh
 fi
