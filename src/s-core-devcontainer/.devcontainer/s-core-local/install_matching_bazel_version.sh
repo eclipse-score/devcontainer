f2e4b708a6db703c57c99dc77732516a48a275ae
lurtz <727209+lurtz@users.noreply.github.com>
2025-11-21 11:55:51 +0100

    Remove USE_BAZEL_VERSION from /etc/profile.d and fix bash completion for bazel >= 8.4.0 (#51)
    * Fix setting USE_BAZEL_VERSION in /etc/profile.d
    
    bazel completions are generated for the bazel version specified with `.bazelversion`. But USE_BAZEL_VERSION was not updated accordingly. This resulted in the wrong bazel version being used and completions not working.
    
    Fixes #48
    
    * avoid installing another bazel version on version mismatch
    
    * do not set USE_BAZEL_VERSION
    
    it override .bazelversion which should not happen by default
    
    * add bash argument if minimum bazel version is met
    
    * fix: add missing parameters
    
    * ensure that bash is used, with fail-fast options
    
    According to https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-syntax#jobsjob_idstepsshell, this should result in
    
    bash --noprofile --norc -eo pipefail {0}
    
    being used.
    
    * break it again to see fail-fast working
    
    * make sur ebash fails fast
    
    cf. https://github.com/devcontainers/ci/issues/294
    
    * fix ci again
    
    * dummy commit to retrigger github actions
    
    * Revert "dummy commit to retrigger github actions"
    
    This reverts commit 6342cc0627b098a9cc26f39db0862aa9a665d8b0.
    
    * Remove the separate build step
    
    Maybe caching does not work as expected, and with this, the publish step at the latest will definitely build the images.
    
    * Force bazel version in tests
    
    * revert workflow changes
    
    should be fixed in separate PR
    
    ---------
    
    Co-authored-by: Oliver Pajonk <oliver.pajonk@elektrobit.com>

--- src/s-core-devcontainer/.devcontainer/s-core-local/install_matching_bazel_version.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/install_matching_bazel_version.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [ -f .bazelversion ] && [ "$(cat .bazelversion)" != "$(bazel version | grep 'Build label:' | awk '{print $3}')" ]; then
+. /devcontainer/features/s-core-local/bazel_setup.sh || true
+
+if [ -f .bazelversion ] && [ "$(cat .bazelversion)" != "$INSTALLED_BAZEL_VERSION" ]; then
     # Pre-install the matching Bazel version, setup the bash command completion
     USE_BAZEL_VERSION=$(cat .bazelversion)
-    bazel help completion > /tmp/bazel-complete.bash
+
+    min_bazel_version_for_bash_option="8.4.0"
+    bash=""
+    if [ "$(printf '%s\n' "$min_bazel_version_for_bash_option" "$USE_BAZEL_VERSION" | sort -V | head -n1)" = "$min_bazel_version_for_bash_option" ]; then
+        bash="bash"
+    fi
+
+    bazel help completion ${bash} > /tmp/bazel-complete.bash
     sudo mv /tmp/bazel-complete.bash /etc/bash_completion.d/bazel-complete.bash
-    sudo sed -i '/^USE_BAZEL_VERSION=/c\USE_BAZEL_VERSION=${USE_BAZEL_VERSION}' /etc/profile.d/bazel.sh || true
+    echo "INSTALLED_BAZEL_VERSION=$USE_BAZEL_VERSION" | sudo tee /devcontainer/features/s-core-local/bazel_setup.sh
 fi
