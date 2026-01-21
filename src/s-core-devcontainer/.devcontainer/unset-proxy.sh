102e649a40d7af70dba77453bbb984b659e5ff74
Emrich Oliver (ETAS) <oliver.emrich2@bosch.com>
2026-01-21 14:55:54 +0000

    Unset proxy environment variables if they are empty

---
+++ src/s-core-devcontainer/.devcontainer/unset-proxy.sh
@@ -0,0 +1,7 @@
+# /etc/profile.d/unset-proxy.sh
+# Unset proxy variables for all login shells if they are empty
+for var in HTTP_PROXY HTTPS_PROXY http_proxy https_proxy NO_PROXY no_proxy; do
+    if [ -z "${!var}" ]; then
+        unset $var
+    fi
+done
