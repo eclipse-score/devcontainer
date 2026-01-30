7fef03c3f117d922f5c0dffad38da5d0d61dba75
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-01-30 20:47:55 +0000

    fix unsetting empty variables

--- src/s-core-devcontainer/.devcontainer/unset-proxy.sh
+++ src/s-core-devcontainer/.devcontainer/unset-proxy.sh
@@ -3,7 +3,7 @@
 # Unset proxy variables for all login shells if they are empty
 for var in HTTP_PROXY HTTPS_PROXY http_proxy https_proxy NO_PROXY no_proxy; do
     eval "value=\${${var}}"
-    if [ -n "${value}" ]; then
+    if [ -z "${value}" ]; then
         unset "${var}"
     fi
 done
