78149bfc91c8d068a82736350ffe4e19e9619bff
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-01-30 17:05:50 +0000

    fix SC1113

--- src/s-core-devcontainer/.devcontainer/unset-proxy.sh
+++ src/s-core-devcontainer/.devcontainer/unset-proxy.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 # /etc/profile.d/unset-proxy.sh
 # Unset proxy variables for all login shells if they are empty
 for var in HTTP_PROXY HTTPS_PROXY http_proxy https_proxy NO_PROXY no_proxy; do
-    if [ -z "${!var}" ]; then
-        unset $var
+    eval "value=\${$var}"
+    if [ -n "$value" ]; then
+        unset "$var"
     fi
 done
