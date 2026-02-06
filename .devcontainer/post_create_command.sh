34b12bbe98c5e480da0eeaabc141d139a5e2667d
lurtz <727209+lurtz@users.noreply.github.com>
2026-02-06 16:49:35 +0100

    chore: check copyright header with opengrep (#78)

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
+
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+
 npm install -g @devcontainers/cli
 pre-commit install
 
 scripts/create_builder.sh
 
 sudo apt-get update && sudo apt-get install -y shellcheck
+
+scripts/install_opengrep.sh
--- src/s-core-devcontainer/.devcontainer/s-core-local/enable_pre_commit_hooks.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/enable_pre_commit_hooks.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************
+
 PRE_COMMIT_CONFIG_FILE=".pre-commit-config.yaml"
 
 if [[ -f "${PRE_COMMIT_CONFIG_FILE}" ]]
