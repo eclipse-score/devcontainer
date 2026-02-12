0135cac7d31b2d09cd49794c2062e31834d14232
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-02-12 16:45:54 +0000

    Remove LLVM and Rust feature
    They increase the image by 2.7GB and might not be needed since bazel or the extensions are able to download the binaries as well.

--- src/s-core-devcontainer/test-project/test.sh
+++ src/s-core-devcontainer/test-project/test.sh
@@ -18,19 +18,6 @@ set -euo pipefail
 
 source "test-utils.sh" vscode
 
-# C++ tooling
-check "validate clangd is working and has the correct version" bash -c "clangd --version | grep '20.1.8'"
-check "validate clang-format is working and has the correct version" bash -c "clang-format --version | grep '20.1.8'"
-check "validate clang-tidy is working and has the correct version" bash -c "clang-tidy --version | grep '20.1.8'"
-check "validate clang is working and has the correct version" bash -c "clang --version | grep '20.1.8'"
-
-# Rust tooling
-check "validate rustc is working and has the correct version" bash -c "rustc --version | grep '1.83.0'"
-check "validate cargo is working and has the correct version" bash -c "cargo --version | grep '1.83.0'"
-check "validate cargo clippy is working and has the correct version" bash -c "cargo clippy --version | grep '0.1.83'"
-check "validate rustfmt is working and has the correct version" bash -c "rustfmt --version | grep '1.8.0-stable'"
-check "validate rust-analyzer is working and has the correct version" bash -c "rust-analyzer --version | grep '1.83.0'"
-
 # Tests from the local s-core-local feature
 source /devcontainer/features/s-core-local/tests/test_default.sh
 
