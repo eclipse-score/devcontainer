cb3f5d61594b5e050648f816954518cd44bb72ed
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-02-12 12:32:14 +0000

    remove deleted and unstaged files from the set of files to annotate

--- scripts/run_reuse_annotate.sh
+++ scripts/run_reuse_annotate.sh
@@ -21,7 +21,15 @@ SCRIPT_DIR=$(dirname -- "${SCRIPT_PATH}")
 
 pushd "${SCRIPT_DIR}/.." > /dev/null
 
+files_to_annotate=$(git ls-files)
+
+# remove deleted files from the list of files to annotate
+deleted_files=$(git ls-files --deleted)
+files_to_annotate=$(comm -23 <(git ls-files | sort) <(echo "${deleted_files}" | sort))
+
+# shellcheck disable=SC2086
+# Expansion of ${files_to_annotate} is intentional to pass the list of files as separate arguments to the reuse annotate command.
 pipx run reuse annotate --template apache-2.0 --merge-copyrights --recursive --skip-unrecognised \
- --copyright="Contributors to the Eclipse Foundation" --license=Apache-2.0 $(git ls-files)
+ --copyright="Contributors to the Eclipse Foundation" --license=Apache-2.0 ${files_to_annotate}
 
 popd > /dev/null
