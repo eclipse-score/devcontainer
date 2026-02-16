b22cce25d68ff718378da9ffdd36dd77f160d2de
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-02-16 16:18:50 +0000

    run hook

--- scripts/run_reuse_annotate.sh
+++ scripts/run_reuse_annotate.sh
@@ -14,6 +14,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # *******************************************************************************
 
+# test
+
 set -euxo pipefail
 
 SCRIPT_PATH=$(readlink -f "$0")
