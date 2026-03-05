95aa032204f368dda72b0e4467093152a498a42f
Lutz Reinhardt <lutz.reinhardt@elektrobit.com>
2026-03-05 13:42:55 +0000

    remove duplicated copright header

--- src/s-core-devcontainer/.devcontainer/s-core-local/install.sh
+++ src/s-core-devcontainer/.devcontainer/s-core-local/install.sh
@@ -11,21 +11,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # *******************************************************************************
+
 # shellcheck disable=SC2016
 # Variables in strings with single quotes are expected to be evaluated later by `eval`
-
-# *******************************************************************************
-# Copyright (c) 2026 Contributors to the Eclipse Foundation
-#
-# See the NOTICE file(s) distributed with this work for additional
-# information regarding copyright ownership.
-#
-# This program and the accompanying materials are made available under the
-# terms of the Apache License Version 2.0 which is available at
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# SPDX-License-Identifier: Apache-2.0
-# *******************************************************************************
 
 set -euo pipefail
 
